ddda2e81a5c958a249417290c04921c69bcfe328
童帝 <runze.crz@alibaba-inc.com>
2019-05-09 22:32:13 +0800

    first blood

---
+++ Example/Pods/Target Support Files/OLFoundation/OLFoundation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OLFoundation : NSObject
+@end
+@implementation PodsDummy_OLFoundation
+@end
---
+++ Example/Pods/Target Support Files/Pods-OLFoundation_Tests/Pods-OLFoundation_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OLFoundation_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_OLFoundation_Tests
+@end
